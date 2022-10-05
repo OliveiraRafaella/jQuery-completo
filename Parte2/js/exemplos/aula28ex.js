b9c2d9295799492e4dbf4579be9aa2534f06c21f
OliveiraRafaella <rafaella.vitorino2001@gmail.com>
2022-10-05 14:06:18 -0300

    SELECIONAR E DESELECIONAR CHEKBOX

---
+++ Parte2/js/exemplos/aula28ex.js
@@ -0,0 +1,21 @@
+/***************************************SELECIONAR E DESELECIONAR CHECKBOXES *********************/
+$(document).ready(function () {
+    $(".toggle").change(function () {
+        if(this.checked){
+            $(":checkbox[name = tecnologia]").attr("checked", "checked")
+        }
+        else{
+            $(":checkbox[name = tecnologia]").removeAttr("checked")
+        }
+    })
+    
+    $(".tudo").click(function () {
+        $(":checkbox[name = tecnologia]").attr("checked", "checked")
+    return false;
+    });
+
+    $(".nada").click(function () {
+        $(":checkbox[name = tecnologia]").removeAttr("checked")
+    return false;
+    });
+});
